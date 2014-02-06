f6fbe257792749e8f65025f6f46546f5ab9f6734
24hoursmedia <info@24hoursmedia.com>
2014-02-06 20:59:20 +0100

    bootstrap from cdn basic system info in homepage

--- src/controllers.php
+++ src/controllers.php
@@ -6,18 +6,50 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig', array('server_name' => $app['config']->getParameter('console.server_name')));
-})
-->bind('homepage')
-;
+$app->get(
+    '/',
+    function () use ($app) {
 
-$app->error(function (\Exception $e, $code) use ($app) {
-    if ($app['debug']) {
-        return;
+        return $app['twig']->render(
+            'index.html.twig',
+            array(
+                '_server' => $_SERVER,
+                'server_name' => $app['config']->getParameter('console.server_name'),
+                'sapi_name' => php_sapi_name(),
+                'uname' => php_uname()
+            )
+        );
     }
+)
+    ->bind('homepage');
 
-    $page = 404 == $code ? '404.html' : '500.html';
 
-    return new Response($app['twig']->render($page, array('code' => $code)), $code);
-});
+$app->get(
+    '/info/phpinfo',
+    function () use ($app) {
+        ob_start();
+        phpinfo();
+        $doc = ob_get_clean();
+        preg_match("/<body[^>]*>(.*?)<\/body>/is", $doc, $matches);
+        $html = $matches[1];
+        $html = str_replace('<table ', '<table class="table table-striped" ', $html);
+        $html = str_replace('class="e"', 'style="width: 30%;font-weight:bold;"', $html);
+        $html = "<small>$html</small>";
+
+        return $app['twig']->render('content.html.twig', array('html' => $html));
+    }
+)
+    ->bind('info_phpinfo');
+
+
+$app->error(
+    function (\Exception $e, $code) use ($app) {
+        if ($app['debug']) {
+            return;
+        }
+
+        $page = 404 == $code ? '404.html' : '500.html';
+
+        return new Response($app['twig']->render($page, array('code' => $code)), $code);
+    }
+);
---
+++ web/test.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eapbachman
+ * Date: 06/02/14
+ * Time: 19:48
+ */
+echo 'hello';
