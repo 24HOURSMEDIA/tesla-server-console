96479021239ef5aa47e65d5c7bfd206c00cda8ec
24hoursmedia <info@24hoursmedia.com>
2014-02-08 12:33:07 +0100

    [add] framework for console configuration

--- config/prod.php
+++ config/prod.php
@@ -1,13 +1,18 @@
 <?php
 
 // configure your app for the production environment
-$app['config'] = $app->share(function() {
+$app['config'] = $app->share(
+    function () {
         $parameterFile = __DIR__ . '/parameters.json';
         $confDir = __DIR__ . '/conf.d';
         $service = new \Tesla\Silex\ConfigurationManager\ConfigurationManager($parameterFile);
-        $service->registerConfigFiles(array(
-            $confDir . '/aws.conf.json',
-        ));
+        $service->registerConfigFiles(
+            array(
+                $confDir . '/aws.conf.json',
+                $confDir . '/tesla-server-console.conf.json',
+            )
+        );
+
         return $service;
     }
 );
